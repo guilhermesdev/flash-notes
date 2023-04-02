9ab21d59fe0d93a3b77616b33582b94e7fc69df5
Guilherme Souza <guilhermesdev.contato@gmail.com>
2023-04-02 20:01:32 -0300

    add daisyui

--- config/dev.exs
+++ config/dev.exs
@@ -16,7 +16,8 @@ config :flash_notes, FlashNotesWeb.Endpoint,
   secret_key_base: "XS9p5TyjBvsrwg5KRW3yEQF+ciC33cdQAuo0ihbve/qzy62VBDADYH0LpdgO9emk",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support
